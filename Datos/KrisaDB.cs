a9c1259b35d4e5c66d6811ec739a12c2b970dd17
Grigory Evropeytsev <codeman222@gmail.com>
2014-05-15 16:16:21 -0500

    Importacion de proyecto inicial

---
+++ Datos/KrisaDB.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Krisa.Datos
+{
+    /// <summary>
+    /// Esta clase representa el contexto de base de datos
+    /// </summary>
+    public class KrisaDB : DbContext
+    {
+        // Agregar clases para persistencia aqui
+        // Ejemplo:
+        // public DbSet<Estudiante> Estudiantes { get; set; }
+
+        public KrisaDB()
+        {
+            RecrearBaseDeDatos();
+        }
+
+        /// <summary>
+        /// Elimina y crea de nuevo base de datos si el modelo ha cambiado.
+        /// </summary>
+        [System.Diagnostics.Conditional("DEBUG")]
+        private void RecrearBaseDeDatos()
+        {
+            Database.SetInitializer<KrisaDB>(new DropCreateDatabaseIfModelChanges<KrisaDB>());
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-krisa
+Solucion Krisa
 =====
+1. Proyecto Datos
+Este proyecto contiene las clases persistentes que van a ser guardados en la
+base de datos. Todos otros proyectos tienen que ocupar las clases de este
+proyecto para guardar y consultar los datos.
+
+2. Proyecto Camara
+Contiene las funciones para trabajar con la cámara de video.
+
+3. Proyecto Tarjeta
+Contiene las funciones de comunicación con la tarjeta.
+
+4. Proyecto ControlUsuarios
+Contiene las operaciones de control de usuarios.
