bd336600328696ab72e592f5f526f2c34f57cdd3
Grigory Evropeytsev <codeman222@gmail.com>
2014-05-17 15:15:52 -0500

    Agregue conexion con la tarjeta y recuperacion de datos fisicos

---
+++ Tarjeta/DriverException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using NationalInstruments.DAQmx;
+
+namespace Krisa.Tarjeta
+{
+    public class DriverException : Exception
+    {
+        internal DriverException(DaqException ex) : base (ex.Message, ex)
+        {
+        }
+    }
+}
---
+++ Tarjeta/Tarjeta.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using NationalInstruments.DAQmx;
+
+namespace Krisa.Tarjeta
+{
+    public class Tarjeta
+    {
+        /// <summary>
+        /// Handle de tarjeta
+        /// </summary>
+        private Device device;
+
+        internal Tarjeta(Device device)
+        {
+            this.device = device;
+        }
+
+        /// <summary>
+        /// Regresa si esta tarjeta es un emulador
+        /// </summary>
+        public bool Emulador
+        {
+            get
+            {
+                try
+                {
+                    return device.IsSimulated;
+                }
+                catch (DaqException ex)
+                {
+                    throw new DriverException(ex);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Regresa los nombres de los canales digitales de esta tarjeta
+        /// </summary>
+        public string[] CanalesDigitales
+        {
+            get
+            {
+                try
+                {
+                    return device.DILines;
+                }
+                catch (DaqException ex)
+                {
+                    throw new DriverException(ex);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Regresa los nombres de los puertos digitales de esta tarjeta
+        /// </summary>
+        public string[] PuertosDigitales
+        {
+            get
+            {
+                try
+                {
+                    return device.DIPorts;
+                }
+                catch (DaqException ex)
+                {
+                    throw new DriverException(ex);
+                }
+            }
+        }
+    }
+}
