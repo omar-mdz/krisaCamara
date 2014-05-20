1684d2e4c88fb9a7e22376ecfab44ea5b6377c8f
Grigory Evropeytsev <codeman222@gmail.com>
2014-05-20 16:17:22 -0500

    Separacion de los parametros de los experimentos

--- Datos/Experimento.cs
+++ Datos/Experimento.cs
@@ -6,30 +6,28 @@ using System.Text;
 namespace Krisa.Datos
 {
     /// <summary>
+    /// El tipo de experimento
+    /// </summary>
+    public enum ExperimentoTipo : int
+    {
+        Entrenamiento,
+        Seguro,
+        Riesgo
+    }
+
+    /// <summary>
     /// Datos del Experimento realizado
     /// </summary>
-    class Experimento
+    public class Experimento
     {
         public int ID { get; set; }
 
         public DateTime FechaHora { get; set; }
 
-        public int Tipo { get; set; }
-
-        public string Observaciones { get; set; }
-
-        public decimal Parametros_Sonido_Frecuencia { get; set; }
-
-        public decimal Parametros_Sonido_Volumen { get; set; }
-
-        public int Parametros_Sonido_Intervalo { get; set; }
-
-        public bool Parametros_Luz { get; set; }
-
-        public int Parametros_NumerosPellets { get; set; }
-
-        public int Parametros_IntervaloParrilla { get; set; }
-
+        public ExperimentoTipo Tipo { get; set; }
         
+        public string Observaciones { get; set; }
+        
+        public ExperimentoParametros Parametros { get; set; }
     }
 }
