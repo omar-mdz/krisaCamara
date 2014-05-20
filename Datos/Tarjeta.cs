44303065b7504f58bed8e1f1f63280ed0041276b
Guadalupe Hernandez <mghh_10@hotmail.com>
2014-05-19 22:35:14 -0500

    Persistencia de las clases ya definidas para tarjeta

---
+++ Datos/Tarjeta.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Krisa.Datos
+{
+    /// <summary>
+    /// Tarjeta existentes en el laboratorio
+    /// </summary>
+
+    public class Tarjeta
+    {
+        public int ID { get; set; }
+
+        public string Nombre { get; set; }
+
+        public string Direccion { get; set; }
+
+        public string Configuracion { get; set; }
+
+    }
+
+    /// <summary>
+    /// Variables utilizas en los Experimentos extraidas de la Tarjeta
+    /// </summary>
+
+    public class VariableDatosTarjeta
+    {
+        public int ID { get; set; }
+
+        public string Nombre { get; set; }
+
+        public string Tipo { get; set; }
+
+        public string Unidad { get; set; }
+
+    }
+}
