c263c9b47638b35f4927e674fda928245a549d65
javier hernandez <xaviher>
2014-05-21 11:53:26 -0500

    se elimino el formulario logueo

--- ControlUsuarioTest/Program.cs
+++ ControlUsuarioTest/Program.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using Krisa.ControlUsuarios;
+
 
 namespace Krisa.ControlUsuarios.Test
 {
@@ -13,24 +13,18 @@ namespace Krisa.ControlUsuarios.Test
         /// Punto de entrada principal para la aplicación.
         /// </summary>
         /// 
-        private static UILogin_Usuario UILogin_Usuario;
-        private static CrearUsuario UIControl_Usuario;
+       
 
         [STAThread]
         static void Main()
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            //Application.Run(new CrearUsuario());
+            Application.Run(new Krisa.ControlUsuarios.CrearUsuario());
 
-            UILogin_Usuario = new UILogin_Usuario();
-            UILogin_Usuario.ShowDialog();
+            
 
-            if (UILogin_Usuario.pbolAccesoCorrecto.Equals(true))
-            {
-                UIControl_Usuario = new CrearUsuario();
-                Application.Run(UIControl_Usuario);
-            }
+          
         }
     }
 }
