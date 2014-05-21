a809d55d5705bde5ccd5cf1067a35144a439e09c
AlfonsoRE <isc_alfonsore@hotmail.com>
2014-05-21 14:01:42 -0500

    Vista controlador

---
+++ ControlUsuarios/ValidarPass.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Krisa.Datos;
+
+namespace Krisa.ControlUsuarios
+{
+    class ValidarPass
+    {
+        public bool VerificarCuenta(string name, string pass)
+        { // clase 
+            bool verifica = true;
+            var context = new KrisaDB();
+
+            Encriptacion encripta = new Encriptacion();
+            string hash = encripta.Encriptar(pass);
+
+            var result = from es in context.Usuarios
+                         where es.Nombre_Usuario == name && es.Pass == hash
+                         select es;
+
+            foreach (var est in result)
+            {
+                verifica = false;
+            }
+
+            return verifica;
+        }
+    }
+}
