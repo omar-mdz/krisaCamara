d6854c44abd1b7669d30ea6fb7709ae0b800b28e
jrodiz <rodiz.cj@gmail.com>
2014-05-21 13:15:12 -0500

    Excepciones agregadas

--- CamaraTest/UIBuscarCamara.cs
+++ CamaraTest/UIBuscarCamara.cs
@@ -135,6 +135,7 @@ namespace CamaraTest
         Bitmap imgsave;
         private void video_NewFrameSave(object sender, NewFrameEventArgs eventArgs)
         { 
+            //
             imgsave = (Bitmap)eventArgs.Frame.Clone();
             try
             {
