b07e9eabd4c667be03071c3fabd2a2513b1efb1b
Grigory Evropeytsev <codeman222@gmail.com>
2014-05-25 00:19:28 -0500

    Metodo adicional para clase ErrorProvider

---
+++ Recursos/ExtensionMethods.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Krisa
+{
+    public static class ExtensionMethods
+    {
+        /// <summary>
+        /// Sets the error description string for the specified control from resource
+        /// </summary>
+        /// <param name="control">The control to set the error description string for</param>
+        /// <param name="resource">The error description resource</param>
+        public static void SetErrorResource(this ErrorProvider errorProvider, Control control, string resource)
+        {
+            errorProvider.SetError(control, Recursos.ResourceManager.GetString(resource));
+        }
+    }
+}
