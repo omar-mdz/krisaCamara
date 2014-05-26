fda2a1bf690b4f6f636c9c92f48105456905e92b
arturosm <arturosm@me.com>
2014-05-25 20:21:34 -0500

    Se implemeto el método de Agregar Camara en el Gestor de Cámara

---
+++ Camara/GestorCamara.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Krisa.Datos;
+
+namespace Krisa.Camara
+{
+    public class GestorCamara
+    {
+        public GestorCamara()
+        {
+
+        }
+
+        public void Agregar(string nombre, string dispositivo)
+        {
+            VideoCamara camara = new VideoCamara();
+            camara.Nombre = nombre;
+            camara.Dispositivo = dispositivo;
+            camara.esActivo = true;
+
+            try
+            {
+                using (var db = new CamaraDB())
+                {
+                    db.Agregar(camara);
+                }
+            }
+            catch (Exception e)
+            {
+                
+                throw new Exception(e.Message);
+            }
+        }
+    }
+}
